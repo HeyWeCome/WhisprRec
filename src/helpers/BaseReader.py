143511dbe4677c2a883bc6c4cbc335e7a4acf8aa
heywecome <heywecome@163.com>
2022-10-05 14:53:05 +0800

    完成基础读取数据类BaseReader

---
+++ src/helpers/BaseReader.py
@@ -0,0 +1,47 @@
+import argparse
+import logging
+
+import numpy as np
+import pandas as pd
+import os
+
+from utils import utils
+
+
+class BaseReader(object):
+    @staticmethod
+    def parse_data_args(self, parser):
+        parser.add_argument('--path', type=str, default='../data/', help='Input data dir.')
+        parser.add_argument('--dataset', type=str, default='Food', help='Choose a dataset.')
+        parser.add_argument('--sep', type=str, default='\t', help='sep of csv file.')
+        return parser
+
+    def __init__(self, args):
+        self.sep = args.sep
+        self.prefix = args.path
+        self.dataset = args.dataset
+        self._read_data()
+
+    # 读取数据 read data
+    def _read_data(self):
+        logging.info('Reading data from \"{}\", dataset = \"{}\" '
+                     .format(self.prefix, self.dataset))
+        # 使用dict存储训练集、验证集和测试集
+        self.data_df = dict()
+        for key in ['train', 'dev', 'test']:
+            self.data_df[key] = pd.read_csv(os.path.join(self.prefix, self.dataset, key + '.csv'), sep=self.sep)
+            self.data_df[key] = utils.eval_list_columns(self.data_df[key])
+
+        logging.info('Counting dataset statistics...')
+        self.all_df = pd.concat([self.data_df[key][['user_id', 'item_id', 'time']]
+                                 for key in ['train', 'dev', 'test']])
+        # 这里也可以使用pd取列的长度
+        self.n_users, self.n_items = self.all_df['user_id'].max() + 1, self.all_df['item_id'].max() + 1
+        for key in ['dev', 'test']:
+            if 'neg_items' in self.data_df[key]:
+                neg_items = np.array(self.data_df[key]['neg_items'].tolist())
+                assert (neg_items >= self.n_items).sum() == 0  # assert negative items don't include unseen ones
+        # entry: 用户与物品的交互
+        logging.info('"# user": {}, "# item": {}, "# entry": {}'.format(
+            self.n_users - 1, self.n_items, len(self.all_df)
+        ))
