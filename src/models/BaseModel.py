0b5bfeffad48d734fd1be82ef7afb1ae222e611b
heywecome <heywecome@163.com>
2022-10-06 14:48:56 +0800

    模型基础父类搭建

---
+++ src/models/BaseModel.py
@@ -0,0 +1,257 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import logging
+import numpy as np
+
+from torch.utils.data import Dataset as BaseDataset
+from torch.nn.utils.rnn import pad_sequence
+from tqdm import tqdm
+from typing import List
+from utils import utils
+from helpers.BaseReader import BaseReader
+from helpers.SeqReader import SeqReader
+
+
+# 基础模型类
+class BaseModel(nn.Module):
+    reader, runner = None, None  # 根据不同的模型选择不同的helper
+    extra_log_args = []  # 需要额外记录的参数
+
+    @staticmethod
+    def parse_model_args(parser):
+        parser.add_argument('--model_path', type=str, default='',
+                            help='Model save path.')
+        parser.add_argument('buffer', type=int, default=1,
+                            help='Whether to buffer feed dicts for dev/test')
+        return parser
+
+    @staticmethod
+    def init_weights(m):
+        """
+        :param m: 使用方差为0，标准差为0.01来进行参数初始化
+        :return:
+        """
+        if 'Linear' in str(type(m)):
+            nn.init.normal_(m.weight, mean=0.0, std=0.01)
+            if m.bias is not None:
+                nn.init.normal_(m.bias, mean=0.0, std=0.01)
+        elif 'Embedding' in str(type(m)):
+            nn.init.normal_(m.weight, mean=0.0, std=0.01)
+
+    def __init__(self, args, corpus: BaseReader):
+        super(BaseReader, self).__init__()
+        self.device = args.device
+        self.model_path = args.model_path
+        self.buffer = args.buffer
+        self.optimizer = None
+        self.check_list = list()  # observe tensors in check_list every check_epoch
+
+    """
+    核心操作:每个模型自己定义
+    """
+
+    def _define_params(self):
+        pass
+
+    def forward(self, feed_dict: dict) -> dict:
+        """
+        :param feed_dict: 数据集
+        :return: out_dict, 要预测的商品列表 [batch_size, n_candidates]
+        """
+        pass
+
+    def loss(self, out_dict: dict) -> torch.Tensor:
+        pass
+
+    """
+    辅助操作
+    """
+
+    def customize_parameters(self) -> list:
+        # customize optimizer settings for different parameters
+        weight_p, bias_p = [], []
+        for name, p in filter(lambda x: x[1].requires_grad, self.named_parameters()):
+            if 'bias' in name:
+                bias_p.append(p)
+            else:
+                weight_p.append(p)
+        optimize_dict = [{'params': weight_p}, {'params': bias_p, 'weight_decay': 0}]
+        return optimize_dict
+
+    def save_model(self, model_path=None):
+        if model_path is None:
+            model_path = self.model_path
+        utils.check_dir(model_path)
+        torch.save(self.state_dict(), model_path)
+        logging.info('Save model to ' + model_path[:50] + '...')
+
+    def load_model(self, model_path=None):
+        if model_path is None:
+            model_path = self.model_path
+        self.load_state_dict(torch.load(model_path))
+        logging.info('Load model from ' + model_path)
+
+    def count_variables(self) -> int:
+        total_parameters = sum(p.numel() for p in self.parameters() if p.requires_grad)
+        return total_parameters
+
+    def actions_after_train(self):  # e.g., save selected parameters
+        pass
+
+    """
+    定义数据集类
+    """
+
+    class Dataset(BaseDataset):
+        def __init__(self, model, corpus, phase: str):
+            self.model = model  # 模型
+            self.corpus = corpus  # reader的reference
+            self.phase = phase  # train / dev / test
+
+            self.buffer_dict = dict()
+            self.data = utils.df_to_dict(corpus.data_df[phase])
+            # ↑ DataFrame 不兼容多线程操作，转为dict
+
+        def __len__(self):
+            if type(self.data) == dict:
+                for key in self.data:
+                    return len(self.data[key])
+            return len(self.data)
+
+        def __getitem__(self, index: int) -> dict:
+            if self.model.buffer and self.phase != 'train':
+                return self.buffer_dict[index]
+            return self._get_feed_dict(index)
+
+        # 关键！：对于一个单例构建输入数据的方法
+        def _get_feed_dict(self, index: int) -> dict:
+            pass
+
+        # 初始化后调用
+        def prepare(self):
+            if self.model.buffer and self.phase != 'train':
+                for i in tqdm(range(len(self)), leave=False, desc=('Prepare ' + self.phase)):
+                    self.buffer_dict[i] = self._get_feed_dict(i)
+
+        # 只在训练阶段之前调用
+        def actions_before_epoch(self):
+            pass
+
+        # Collate a batch according to the list of feed dicts
+        def collate_batch(self, feed_dicts: List[dict]) -> dict:
+            feed_dict = dict()
+            for key in feed_dicts[0]:
+                if isinstance(feed_dicts[0][key], np.ndarray):
+                    tmp_list = [len(d[key]) for d in feed_dicts]
+                    if any([tmp_list[0] != l for l in tmp_list]):
+                        stack_val = np.array([d[key] for d in feed_dicts], dtype=np.object)
+                    else:
+                        stack_val = np.array([d[key] for d in feed_dicts])
+                else:
+                    stack_val = np.array([d[key] for d in feed_dicts])
+                if stack_val.dtype == np.object:  # inconsistent length (e.g., history)
+                    feed_dict[key] = pad_sequence([torch.from_numpy(x) for x in stack_val], batch_first=True)
+                else:
+                    feed_dict[key] = torch.from_numpy(stack_val)
+            feed_dict['batch_size'] = len(feed_dicts)
+            feed_dict['phase'] = self.phase
+            return feed_dict
+
+
+class GeneralModel(BaseModel):
+    reader, runner = 'BaseReader', 'BaseRunner'
+
+    @staticmethod
+    def parse_model_args(parser):
+        parser.add_argument('--num_neg', type=int, default=1,
+                            help='The number of negative items during training.')
+        parser.add_argument('--dropout', type=float, default=0,
+                            help='Dropout probability for each deep layer')
+        parser.add_argument('--test_all', type=int, default=0,
+                            help='Whether testing on all the items.')
+        return BaseModel.parse_model_args(parser)
+
+    def __init__(self, args, corpus):
+        super().__init__(args, corpus)
+        self.user_num = corpus.n_users
+        self.item_num = corpus.n_items
+        self.num_neg = args.num_neg
+        self.dropout = args.dropout
+        self.test_all = args.test_all
+
+    def loss(self, out_dict: dict) -> torch.Tensor:
+        """
+        BPR ranking loss with optimization on multiple negative samples (a little different now)
+        "Recurrent neural networks with top-k gains for session-based recommendations"
+        :param out_dict: contain prediction with [batch_size, -1], the first column for positive, the rest for negative
+        :return:
+        """
+        predictions = out_dict['prediction']
+        pos_pred, neg_pred = predictions[:, 0], predictions[:, 1:]
+        neg_softmax = (neg_pred - neg_pred.max()).softmax(dim=1)
+        loss = -((pos_pred[:, None] - neg_pred).sigmoid() * neg_softmax).sum(dim=1).log().mean()
+        # neg_pred = (neg_pred * neg_softmax).sum(dim=1)
+        # loss = F.softplus(-(pos_pred - neg_pred)).mean()
+        # ↑ For numerical stability, use 'softplus(-x)' instead of '-log_sigmoid(x)'
+        return loss
+
+    class Dataset(BaseModel.Dataset):
+        def _get_feed_dict(self, index: int) -> dict:
+            user_id, target_item = self.data['user_id'][index], self.data['item_id'][index]
+            if self.phase != 'train' and self.model.test_all:
+                neg_items = np.arange(1, self.corpus.n_items)
+            else:
+                neg_items = self.data['neg_items'][index]
+            item_ids = np.concatenate([[target_item], neg_items]).astype(int)
+            feed_dict = {
+                'user_id': user_id,
+                'item_id': item_ids
+            }
+            return feed_dict
+
+        # Sample negative items for all the instances
+        def actions_before_epoch(self):
+            # 取数字在(1, self.corpus.n_items)之间，生成[len(self), self.model.num_neg]的形状
+            neg_items = np.random.randint(1, self.corpus.n_items, size=(len(self), self.model.num_neg))
+            for i, u in enumerate(self.data['user_id']):
+                clicked_set = self.corpus.train_clicked_set[u]  # neg items are possible to appear in dev/test set
+                # clicked_set = self.corpus.clicked_set[u]  # neg items will not include dev/test set
+                for j in range(self.model.num_neg):
+                    while neg_items[i][j] in clicked_set:
+                        neg_items[i][j] = np.random.randint(1, self.corpus.n_items)
+            self.data['neg_items'] = neg_items
+
+
+# 序列模型的Model
+class SequentialModel(GeneralModel):
+    # reader = 'BaseReader'
+    reader = 'SeqReader'
+
+    @staticmethod
+    def parse_model_args(parser):
+        parser.add_argument('--history_max', type=int, default=20,
+                            help='Maximum length of history.')
+        return GeneralModel.parse_model_args(parser)
+
+    def __init__(self, args, corpus):
+        super().__init__(args, corpus)
+        self.history_max = args.history_max
+
+    class Dataset(GeneralModel.Dataset):
+        def __init__(self, model, corpus, phase):
+            super().__init__(model, corpus, phase)
+            idx_select = np.array(self.data['position']) > 0  # history length must be non-zero
+            for key in self.data:
+                self.data[key] = np.array(self.data[key])[idx_select]
+
+        def _get_feed_dict(self, index):
+            feed_dict = super()._get_feed_dict(index)
+            pos = self.data['position'][index]
+            user_seq = self.corpus.user_his[feed_dict['user_id']][:pos]
+            if self.model.history_max > 0:
+                user_seq = user_seq[-self.model.history_max:]
+            feed_dict['history_items'] = np.array([x[0] for x in user_seq])
+            feed_dict['history_times'] = np.array([x[1] for x in user_seq])
+            feed_dict['lengths'] = len(feed_dict['history_items'])
+            return feed_dict
