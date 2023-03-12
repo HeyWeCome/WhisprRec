669cf0514890f8273e135827975ea3989442cfef
heywecome <heywecome@163.com>
2023-03-12 14:23:38 +0800

    [bug fixed]remove 'developing' and 'graph' package.

--- src/main.py
+++ src/main.py
@@ -7,8 +7,8 @@ import pandas as pd
 import torch
 
 from helpers import *
-from models.developing import *
-from models.graph import *
+# from models.developing import *
+# from models.graph import *
 from models.general import *
 from models.sequential import *
 from utils import utils
@@ -100,7 +100,7 @@ def save_rec_results(dataset, runner, topk):
 
 if __name__ == '__main__':
     init_parser = argparse.ArgumentParser(description='Model')
-    init_parser.add_argument('--model_name', type=str, default='SE2RG', help='Choose a model to run.')
+    init_parser.add_argument('--model_name', type=str, default='TiSASRec', help='Choose a model to run.')
     init_args, init_extras = init_parser.parse_known_args()
     model_name = eval('{0}.{0}'.format(init_args.model_name))
     reader_name = eval('{0}.{0}'.format(model_name.reader))  # model chooses the reader
