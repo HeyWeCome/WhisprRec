08d7c34d88b86514f0ec4dbf1f5dbccd2a6211b7
heywecome <heywecome@163.com>
2023-04-04 09:03:35 +0800

    [Update]Restruct KGReader.py

--- src/helpers/KGReader.py
+++ src/helpers/KGReader.py
@@ -29,15 +29,15 @@ class KGReader(SeqReader):
         self._construct_kg()
 
     def _construct_kg(self):
-        logging.info('构建关系元组中...')
+        logging.info('Start building relational tuples')
 
         self.triplet_set = set()
         heads, relations, tails = [], [], []
 
-        # 以r开头的，为数据集中存在的关系
+        # Beginning with r, the relationship exists in the dataset
         self.item_relations = [r for r in self.item_meta_df.columns if r.startswith('r_')]
-        for idx in trange(len(self.item_meta_df), desc='构建物品关系中'):
-            head_item = self.item_meta_df['item_id'].values[idx]
+        for idx in trange(len(self.item_meta_df), desc='Start building Item-Item relational tuples'):
+            head_item = self.item_meta_df['item_id'].values[idx]  # equals to self.item_meta_df['item_id'][idx]
             for r_idx, r in enumerate(self.item_relations):
                 for tail_item in self.item_meta_df[r].values[idx]:
                     heads.append(head_item)
@@ -46,14 +46,16 @@ class KGReader(SeqReader):
                     self.triplet_set.add((head_item, r_idx + 1, tail_item))
         logging.info('Item-item relations:' + str(self.item_relations))
 
-        # 基于属性的关系
+        # Attribute-based relationships
         self.attr_relations = list()
         if self.include_attr:
             self.attr_relations = [r for r in self.item_meta_df.columns if r.startswith('i_')]
             self.attr_max, self.share_attr_dict = list(), dict()
-            for r_idx, attr in tqdm(enumerate(self.attr_relations), desc='构建属性关系中'):
+            for r_idx, attr in tqdm(enumerate(self.attr_relations), desc='Start building attribute-based relational '
+                                                                         'tuples'):
                 base = self.n_items + np.sum(self.attr_max)  # base index of attribute entities
                 relation_idx = len(self.item_relations) + r_idx + 1  # index of the relation type
+                # Essentially taking out a line
                 for item, val in zip(self.item_meta_df['item_id'], self.item_meta_df[attr]):
                     if val != 0:  # the attribute is not NaN
                         heads.append(item)
@@ -71,6 +73,7 @@ class KGReader(SeqReader):
         self.relation_df['relation'] = relations
         self.relation_df['tail'] = tails
         self.n_relations = len(self.relations) + 1
+        # Put head and tail together, then take the number + 1
         self.n_entities = pd.concat((self.relation_df['head'], self.relation_df['tail'])).max() + 1
         logging.info('"# relation": {}, "# triplet": {}'
                      .format(self.n_relations, len(self.relation_df)))
