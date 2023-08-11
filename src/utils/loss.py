ee16aade1958486574801aad7871bd80e5ab298c
heywecome <heywecome@163.com>
2023-08-11 17:09:28 +0800

    Add EmbLoss

--- src/utils/loss.py
+++ src/utils/loss.py
@@ -36,3 +36,28 @@ class BPRLoss(nn.Module):
     def forward(self, pos_score, neg_score):
         loss = -torch.log(self.gamma + torch.sigmoid(pos_score - neg_score)).mean()
         return loss
+
+
+class EmbLoss(nn.Module):
+    """EmbLoss, regularization on embeddings"""
+
+    def __init__(self, norm=2):
+        super(EmbLoss, self).__init__()
+        self.norm = norm
+
+    def forward(self, *embeddings, require_pow=False):
+        if require_pow:
+            emb_loss = torch.zeros(1).to(embeddings[-1].device)
+            for embedding in embeddings:
+                emb_loss += torch.pow(
+                    input=torch.norm(embedding, p=self.norm), exponent=self.norm
+                )
+            emb_loss /= embeddings[-1].shape[0]
+            emb_loss /= self.norm
+            return emb_loss
+        else:
+            emb_loss = torch.zeros(1).to(embeddings[-1].device)
+            for embedding in embeddings:
+                emb_loss += torch.norm(embedding, p=self.norm)
+            emb_loss /= embeddings[-1].shape[0]
+            return emb_loss
