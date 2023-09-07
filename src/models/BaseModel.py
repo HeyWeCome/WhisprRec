abf6b47879ab655c2415baf1e8dd7a5d189b227d
heywecome <heywecome@163.com>
2023-09-07 10:51:20 +0800

    Fix the bug caused by the number of user and item.

--- src/models/BaseModel.py
+++ src/models/BaseModel.py
@@ -133,8 +133,8 @@ class BaseModel(nn.Module):
 class GeneralModel(BaseModel):
     def __init__(self, corpus, configs):
         super().__init__(corpus, configs)
-        self.user_num = corpus.n_users + 1
-        self.item_num = corpus.n_items + 1
+        self.user_num = int(corpus.n_users) + 1
+        self.item_num = int(corpus.n_items) + 1
         self.num_neg = configs['runner']['num_neg']
         self.test_all = configs['runner']['test_all']
 
